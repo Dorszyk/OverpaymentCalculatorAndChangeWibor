5fef19915c115ba748590e6cb851d4eadc85f6b2
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2024-04-14 19:02:03 +0200

    Logback.logs&DiagramGenerator.overpayment.puml&Gradle8.5

--- src/main/java/pl/inbank/overpayment/services/ConstantAmountsCalculationServiceImpl.java
+++ src/main/java/pl/inbank/overpayment/services/ConstantAmountsCalculationServiceImpl.java
@@ -18,34 +18,43 @@ public class ConstantAmountsCalculationServiceImpl implements ConstantAmountsCal
     @Override
     public InstallmentAmounts calculate(final InputData inputData, final Overpayment overpayment) {
         BigDecimal interestPercent = inputData.interestPercent();
-        log.debug("InterestPercent: [{}]", interestPercent);
+        log.info("InterestPercent: [{}]", interestPercent);
 
         BigDecimal residualAmount = inputData.amount();
+        log.info("ResidualAmount: [{}]", residualAmount);
 
         // Ustalenie dat startowych i końcowych dla obliczenia odsetek
-        LocalDate previousPaymentDate = inputData.repaymentStartDate(); // Zakładamy, że jest to data rozpoczęcia kredytu
+        LocalDate previousPaymentDate = inputData.repaymentStartDate();// Zakładamy, że jest to data rozpoczęcia kredytu
+        log.info("PreviousPaymentDate: [{}]",previousPaymentDate);
+
         LocalDate currentPaymentDate = inputData.repaymentStartDate().plusMonths(1); // Data następnej spłaty jako punkt końcowy
+        log.info("CurrentPaymentDate: [{}]",currentPaymentDate);
 
         // Obliczenie kwoty odsetek z uwzględnieniem rzeczywistych dni
         BigDecimal interestAmount = AmountsCalculationService.calculateInterestAmount(
                 residualAmount, interestPercent, previousPaymentDate, currentPaymentDate);
+        log.info("InterestAmount: [{}]",interestAmount);
 
         BigDecimal q = AmountsCalculationService.calculateQ(interestPercent);
-        log.trace("Q: [{}]", q);
+        log.info("Q: [{}]", q);
 
         // Obliczenie stałej kwoty raty bez uwzględnienia prowizji
         BigDecimal installmentAmountWithoutCommission = calculateConstantInstallmentAmount(
                 q, interestAmount, residualAmount, inputData.amount(), inputData.monthsDuration());
+        log.info("InstallmentAmountWithoutCommission: [{}]",installmentAmountWithoutCommission);
 
         // Obliczenie kwoty kapitałowej jako różnicy między całkowitą kwotą raty (bez prowizji) a kwotą odsetek
         BigDecimal capitalAmount = AmountsCalculationService.compareCapitalWithResidual(
                 installmentAmountWithoutCommission.subtract(interestAmount), residualAmount);
+        log.info("CapitalAmount: [{}]",capitalAmount);
 
         // Pobranie opłaty prowizyjnej
         BigDecimal commissionAmount = inputData.monthlyCommissionFee();
+        log.info("CommissionAmount: [{}]",commissionAmount);
 
         // Dodanie opłaty prowizyjnej do całkowitej kwoty raty
         BigDecimal installmentAmount = installmentAmountWithoutCommission.add(commissionAmount);
+        log.info("InstallmentAmount: [{}]",installmentAmount);
 
         // Logowanie obliczeń
         log.info(
@@ -60,37 +69,55 @@ public class ConstantAmountsCalculationServiceImpl implements ConstantAmountsCal
     public InstallmentAmounts calculate(final InputData inputData, final Overpayment overpayment, final Installment previousInstallment) {
         // Pobranie aktualnej wartości WIBOR dla bieżącej raty
         BigDecimal currentInstallmentNumber = previousInstallment.installmentNumber().add(BigDecimal.ONE);
+        log.info("CurrentInstallmentNumber: [{}]", currentInstallmentNumber);
+
         BigDecimal wiborPercent = inputData.wiborPercentForInstallment(previousInstallment.installmentNumber().add(BigDecimal.ONE));
+        log.info("WiborPercent: [{}]", wiborPercent);
+
         BigDecimal interestPercent = wiborPercent.add(inputData.marginPercent()); // Dodanie marży do WIBOR
+        log.info("interestPercent: [{}]", interestPercent);
 
         BigDecimal q = AmountsCalculationService.calculateQ(interestPercent);
+        log.info("q: [{}]", q);
+
 
         BigDecimal residualAmount = previousInstallment.residual().residualAmount();
+        log.info("residualAmount: [{}]", residualAmount);
 
         LocalDate previousPaymentDate, currentPaymentDate;
         previousPaymentDate = inputData.repaymentStartDate().plusMonths(currentInstallmentNumber.intValue() - 1);
-        currentPaymentDate = inputData.repaymentStartDate().plusMonths(currentInstallmentNumber.intValue());
+        log.info("previousPaymentDate: [{}]", previousPaymentDate);
 
+        currentPaymentDate = inputData.repaymentStartDate().plusMonths(currentInstallmentNumber.intValue());
+        log.info("residualAmount: [{}]", residualAmount);
 
         // Obliczenie kwoty odsetek z uwzględnieniem aktualnej wartości WIBOR oraz rzeczywistej liczby dni
         BigDecimal interestAmount = AmountsCalculationService.calculateInterestAmount(
                 residualAmount, interestPercent, previousPaymentDate, currentPaymentDate);
+        log.info("interestAmount: [{}]", interestAmount);
 
         BigDecimal referenceAmount = previousInstallment.reference().referenceAmount();
+        log.info("referenceAmount: [{}]", referenceAmount);
+
         BigDecimal referenceDuration = previousInstallment.reference().referenceDuration();
+        log.info("referenceDuration: [{}]", referenceDuration);
 
         // Obliczenie stałej kwoty raty bez uwzględnienia prowizji
         BigDecimal installmentAmountWithoutCommission = calculateConstantInstallmentAmount(
                 q, interestAmount, residualAmount, referenceAmount, referenceDuration);
+        log.info("installmentAmountWithoutCommission: [{}]", installmentAmountWithoutCommission);
 
         // Obliczenie kwoty kapitałowej jako różnicy między całkowitą kwotą raty (bez prowizji) a kwotą odsetek
         BigDecimal capitalAmount = AmountsCalculationService.compareCapitalWithResidual(installmentAmountWithoutCommission.subtract(interestAmount), residualAmount);
-
+        log.info("capitalAmount: [{}]", capitalAmount);
         // Pobranie opłaty prowizyjnej
         BigDecimal commissionAmount = inputData.monthlyCommissionFee();
+        log.info("commissionAmount: [{}]", commissionAmount);
+
 
         // Dodanie opłaty prowizyjnej do całkowitej kwoty raty
         BigDecimal installmentAmount = installmentAmountWithoutCommission.add(commissionAmount);
+        log.info("installmentAmount: [{}]", installmentAmount);
 
         log.info(
                 "Calculated installment: [{}], residualAmount: [{}], interestAmount: [{}], capitalAmount: [{}], installmentAmount: [{}], " +
